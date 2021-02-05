9a6be7bae0508a9a8e6f7ffc0fea6c3bdc1dd257
Scott Cole <scott.cole@skybettingandgaming.com>
2021-02-05 15:52:19 +0000

    First Commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import { Article } from "@components/Article";
 export default function About() {
     return (
         <Article>
-            <h1>About Me</h1>
+            <h1>About Me dude</h1>
             <p>This is a sick blog</p>
         </Article>
     );
